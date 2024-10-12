432bfff543eb32a3d487e8394a845418e7eead4f
Dalia Grodzka <39556362+NiyeSadece@users.noreply.github.com>
2024-10-13 00:04:16 +0200

    npc detectig player & base for dialogue

---
+++ Assets/Scripts/NPCSystem.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCSystem : MonoBehaviour
+{
+    bool playerDetected = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerDetected && Input.GetKeyDown(KeyCode.F))
+        {
+            Debug.Log("Dialogue started");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player") playerDetected = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+       playerDetected = false;
+
+    }
+}
