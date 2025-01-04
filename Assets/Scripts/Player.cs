a3dcfed4385c3b270eac30cca5a7d0f3a579816e
paniczeciak <kasia.trz98@wp.pl>
2025-01-04 18:50:14 +0100

    Run Animation & Player movement Script Updated

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,44 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
-
 {
-    [SerializeField] private float speed = 2f;
-    [SerializeField] private float jumpForce = 5f;  // Wysokoœæ skoku
+    [SerializeField] private float walkSpeed = 2f;  // Prêdkoœæ chodzenia
+    [SerializeField] private float runSpeed = 4f;  // Prêdkoœæ biegania
+    [SerializeField] private float jumpForce = 5f; // Wysokoœæ skoku
     private Animator animator;
     private Rigidbody rb;
-    private bool isGrounded;
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody>();
-        // Upewnij siê, ¿e Rigidbody nie jest kinetyczne
         rb.isKinematic = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Sprawdzanie, czy `Shift` jest wciœniêty
+        bool isRunning = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+        float currentSpeed = isRunning ? runSpeed : walkSpeed;
+
         // Ruch w osi X
         float horizontalInput = Input.GetAxis("Horizontal");  // Wartoœci -1 dla "A", 1 dla "D"
-        Vector3 velocity = new Vector3(horizontalInput * speed, rb.velocity.y, 0f);  // Ruch po osi X
+        Vector3 velocity = new Vector3(horizontalInput * currentSpeed, rb.velocity.y, 0f);  // Ruch po osi X
 
         // Zastosowanie prêdkoœci do Rigidbody
         rb.velocity = velocity;
 
-        // Aktualizowanie animacji na podstawie prêdkoœci
-        animator.SetFloat("Speed", Mathf.Abs(rb.velocity.x));
+        // Aktualizowanie parametru Speed w Animatorze
+        float animationSpeed = Mathf.Abs(rb.velocity.x);
+        animator.SetFloat("Speed", animationSpeed);
 
-        // Sprawdzanie, czy gracz dotyka ziemi, aby skaka³
-        isGrounded = Physics.Raycast(transform.position, Vector3.down, 0.1f);  // Sprawdzamy, czy na ziemi
-
-        if (isGrounded && Input.GetKeyDown(KeyCode.W))  // Skok
+        // Skok
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             rb.velocity = new Vector3(rb.velocity.x, jumpForce, 0f);  // Dodajemy prêdkoœæ w osi Y
             animator.SetTrigger("Jump");  // Wywo³anie animacji skoku (jeœli jest ustawiona)
-            Debug.Log("Skok");
         }
 
         // Obracanie postaci w zale¿noœci od kierunku
@@ -54,63 +54,3 @@ public class Player : MonoBehaviour
         }
     }
 }
-
-//{
-//    [SerializeField] private float speed = 2f;
-
-//    private Animator animator;
-
-//    // Start is called before the first frame update
-//    void Start()
-//    {
-//        animator = GetComponent<Animator>();
-//    }
-
-//    // Update is called once per frame
-//    void Update()
-//    {
-//        // Ruch w osi X
-//        float horizontalInput = Input.GetAxis("Horizontal");  // Wartoœci -1 dla "A", 1 dla "D"
-//        var velocity = Vector3.right * horizontalInput * speed;  // Ruch po osi X (lewo/prawo)
-
-//        // Aktualizowanie animacji na podstawie prêdkoœci
-//        animator.SetFloat("Speed", Mathf.Abs(velocity.x));
-
-//        // Translacja postaci
-//        transform.Translate(velocity * Time.deltaTime, Space.World);
-
-//        // Obrót postaci
-//        if (horizontalInput > 0)
-//        {
-//            // Obrót w prawo
-//            transform.rotation = Quaternion.Euler(0f, 90f, 0f);
-//        }
-//        else if (horizontalInput < 0)
-//        {
-//            // Obrót w lewo
-//            transform.rotation = Quaternion.Euler(0f, -90f, 0f);
-//        }
-//    }
-//}
-
-
-//{
-//    [SerializeField] private float speed = 2f;
-
-//    private Animator animator;
-
-//    // Start is called before the first frame update
-//    void Start()
-//    {
-//        animator = GetComponent<Animator>();
-//    }
-
-//    // Update is called once per frame
-//    void Update()
-//    {
-//        var velocity = Vector3.forward * Input.GetAxis("Horizontal") * speed;
-
-//        transform.Translate(velocity * Time.deltaTime);
-//        animator.SetFloat("Speed", velocity.z);
-//    }
-//}
